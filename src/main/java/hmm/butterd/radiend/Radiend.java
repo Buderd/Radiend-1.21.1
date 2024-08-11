f670bf0d928358985b53fb26d147e12d58371c9c
Damian Ochoa <damianochoa@Damians-MacBook-Air.local>
2024-08-11 13:24:50 -0400

    Initial commit

---
+++ src/main/java/hmm/butterd/radiend/Radiend.java
@@ -0,0 +1,20 @@
+package hmm.butterd.radiend;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Radiend implements ModInitializer {
+	public static final String MOD_ID = "radiend";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Viva la Radiation!");
+	}
+}
---
+++ src/main/java/hmm/butterd/radiend/RadiendClient.java
@@ -0,0 +1,10 @@
+package hmm.butterd.radiend;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RadiendClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
